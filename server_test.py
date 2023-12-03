d6e36e6353d9f9419ca30f455ad36414ae08c834
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2023-12-03 13:58:18 -0500

    Add unit test for data service

---
+++ server_test.py
@@ -0,0 +1,27 @@
+# Test for HTTP GET requests to the server
+
+import requests
+
+print("\nTesting data service... ", end="")
+
+# Test hello world
+response = requests.get("http://localhost:3000/")
+assert response.status_code == 200
+print("0 ", end="")
+
+# Test basic user authentication
+response = requests.get("http://localhost:3000/user", auth=("admin", "admin"))
+assert response.status_code == 200
+print("1 ", end="")
+
+# Test getting a list of all phrases from user 1
+response = requests.get("http://localhost:3000/user/1/phrase")
+assert response.status_code == 200
+print("2 ", end="")
+
+# Test getting phrase 1 from user 1
+response = requests.get("http://localhost:3000/user/1/phrase/1")
+assert response.status_code == 200
+print("3 ", end="")
+
+print("OK!\n")
